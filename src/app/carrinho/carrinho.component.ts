79d779f4034ffb5b5ffecebc9602f41b6c5aa428
Joadson <joadsondaniel@gmail.com>
2023-02-22 11:34:22 -0300

    build

---
+++ src/app/carrinho/carrinho.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
+import { ProdutosComponent } from '../produtos/produtos.component';
+
+@Component({
+  selector: 'app-carrinho',
+  templateUrl: './carrinho.component.html',
+  styleUrls: ['./carrinho.component.css']
+})
+export class CarrinhoComponent implements OnInit{
+
+  itensCarrinho: IProdutoCarrinho[] = []
+  total = 0;
+
+  constructor(
+    public carrinhoService: CarrinhoService,
+    private router: Router
+
+  ){
+
+  }
+
+  ngOnInit(): void{
+
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho()
+    this.calcularTotal()
+
+  }
+
+  calcularTotal(){
+    this.total = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.quantidade), 0)
+    
+
+}
+
+  removeProdutoCarrinho(produtoId: number){
+    this.itensCarrinho = this.itensCarrinho.filter(items => items.id !== produtoId)
+    this.carrinhoService.removerProdutoCarrinho(produtoId)
+    this.calcularTotal()
+  }
+
+  comprar(){
+    alert("Parabéns, você finalizou sua compra")
+    this.carrinhoService.limparCarrinho()
+    this.router.navigate(["produtos"])
+  }
+  }
+
+
